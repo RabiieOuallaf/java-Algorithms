823d3903ee2d0676ef8d2d226c8b153f0150bff8
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-05-28 20:21:28 +0100

    Linear search

---
+++ src/LinearSearch.java
@@ -0,0 +1,23 @@
+
+public class LinearSearch {
+    // Linear serach  = Iterate through a collection one element at a time
+    LinearSearch(int value) {
+        int[] array = {9,5,4,3,7,8,2,12,98,33,656,743,0};
+        int index = linearSearch(array, value);
+
+        if(index != -1){
+            System.out.print("Element found on index :" +index);
+        }else {
+            System.out.println("Sorry, this element does not exist");
+        }
+    }
+
+    private static int linearSearch(int[] array, int value) {
+        for(int i = 0; i < array.length;i++) {
+            if(array[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
